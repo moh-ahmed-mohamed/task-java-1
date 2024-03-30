fbadad74f8bf4244322b635bcd62c5fe9c079b25
mohamedabdelaal <153386966+moh-ahmed-mohamed@users.noreply.github.com>
2024-03-30 17:13:58 +0200

    Add files via upload

---
+++ text.js
@@ -0,0 +1,68 @@
+// // document.getElementById('moh').innerHTML = 'hello world';
+
+
+// var my_nmae ='mohamed';
+// var my_age =24;
+// var my_gob ='front end';
+
+
+// var my_developers_one = 'mohamed';
+// var my_developers_two = 'ahmed';
+// var my_developers_there = 'hassan';
+// var my_developers_four = 'mahmoud';
+// var my_developers_five = 'ali';
+// var my_developers_six = 'alaa';
+// var my_developers_seven = 'karime';
+
+// document.getElementById('moh').innerHTML = 'mydevelopers'+ ' ' + my_developers_one + ' ' + my_developers_two + ' ' + my_developers_there + ' ' + my_developers_four ;
+
+// var my_car = {
+//     color: 'red',
+//     price:300,
+//     model:2024,
+//     owner_name:'mohamed'
+    
+// }
+// document.getElementById('moh').innerHTML = my_car;
+// var x =100;
+// var y =1000;
+// var total = x + y ;
+// document.write(total);
+
+// var my_name = 'mohamed abdelaal'
+// var x =100;
+// var u =100;
+
+// if(x != u){
+//     console.log('right');
+// }else if (x >= u){
+//     console.log('good');
+// }else {
+//     console.log('hellow');
+// }
+var a = 10;
+var b = 20;
+var c = 30;
+var d = 40;
+var e = 50;
+var f = 60;
+var j = 70;
+var h = 80;
+var k = 90;
+var l = 100;
+var m = 110;
+var l = 120;
+var p = 130;
+var o = 140;
+var i = 150;
+var u = 160;
+var y = 170;
+var t = 180;
+var r = 190;
+var w = 200;
+
+var total = a+b+c+d+e+f+j+h+k+l+m+p+o+i+u+y+t+r+w;
+
+var totall = total/20 ;
+
+document.getElementById('moh').innerHTML= totall ;
